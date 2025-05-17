4dd0a17dacde7515129c07da72ae21f6a76cc97e
hst1189 <hst1189@hotmail.com>
2025-05-17 23:44:35 +0900

    Update musicPlayer.js

--- assets/js/musicPlayer.js
+++ assets/js/musicPlayer.js
@@ -1,10 +1,15 @@
+const music_div = document.createElement('div')
+music_div.id = 'player'   // 设置元素id
+document.body.appendChild(music_div)  // 插入到body元素最后
+
 const ap = new APlayer({
-	container: document.getElementById('player'),
+    container: document.getElementById('player'),
     fixed: true,
     audio: [{
         name: 'name',
         artist: 'artist',
         url: '../music/TimeAfterTime.mp3',
         cover: 'images/other/11056086_074392.jpg',
-	}]
+    }]
 });
+

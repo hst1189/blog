cc5a690f09872115c91847e8f81f853c98233bea
hst1189 <hst1189@hotmail.com>
2025-05-31 23:54:28 +0900

    Update musicPlayer.js

--- assets/js/musicPlayer.js
+++ assets/js/musicPlayer.js
@@ -1,5 +1,6 @@
 const music_div = document.createElement('div')
 music_div.id = 'music_player'   // 设置元素id
+music_div.style = 'width: 80%' 
 // document.body.appendChild(music_div)  // 插入到body元素最后
 document.getElementsByClassName('about')[0].appendChild(music_div)
 
